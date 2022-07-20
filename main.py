64c846fcf45f6749547aa9075f2fdb3e44b0480e
sam <vulmail@yahoo.com>
2022-07-19 21:23:08 -0700

    # THEMES https://www.youtube.com/watch?v=1CiqhfbeKPQ

--- main.py
+++ main.py
@@ -134,6 +134,8 @@ MainApp().run()
 
 # TEXT FIELD
 
+"""
+
 from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
 from kivymd.uix.textfield import MDTextField
@@ -152,6 +154,37 @@ class DemoApp(MDApp):
                                pos_hint={'center_x': 0.5, 'center_y': 0.5},
                                size_hint_x=None, width=200)
         screen.add_widget(username)
+        return screen
+
+
+DemoApp().run()
+
+"""
+# THEMES
+
+'''
+1) What is a theme?
+2) primary_palette on buttons
+3) Color Options in primary_palette - Available options are: ‘Red’, ‘Pink’, ‘Purple’, ‘DeepPurple’, ‘Indigo’, ‘Blue’, ‘LightBlue’, ‘Cyan’, ‘Teal’, ‘Green’, ‘LightGreen’, ‘Lime’, ‘Yellow’, ‘Amber’, ‘Orange’, ‘DeepOrange’, ‘Brown’, ‘Gray’, ‘BlueGray’.
+4) Primary hue option - ‘50’, ‘100’, ‘200’, ‘300’, ‘400’, ‘500’, ‘600’, ‘700’, ‘800’, ‘900’, ‘A100’, ‘A200’, ‘A400’, ‘A700’.
+5) theme_style - Dark or Light two options
+'''
+
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivymd.uix.button import MDRectangleFlatButton
+
+
+class DemoApp(MDApp):
+
+    def build(self):
+        self.theme_cls.primary_palette = "Orange"
+        self.theme_cls.primary_hue = "100"
+        self.theme_cls.theme_style = "Dark"
+        screen = Screen()
+        btn_flat = MDRectangleFlatButton(text='Hello World',
+                                         pos_hint={'center_x': 0.5, 'center_y': 0.5})
+        screen.add_widget(btn_flat)
         return screen
 
 
